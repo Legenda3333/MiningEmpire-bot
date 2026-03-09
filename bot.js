46ebef68d989a581a59ccfec5c24290f61c872bd
Артём <legenda333aa@gmail.com>
2026-03-09 21:17:25 +0300

    update

--- bot.js
+++ bot.js
@@ -8,28 +8,27 @@ const bot = new Telegraf(process.env.TOKEN);
 
 
 bot.start(async (ctx) => {
-  await ctx.telegram.sendPhoto(
-    ctx.chat.id,
-    'https://raw.githubusercontent.com/Legenda3333/MiningEmpire-frontend/main/images/start_image.png',
-    {
-      caption:
-        '👋 <b>Добро пожаловать в MiningEmpire!</b> 🚀\n\n' +
-        '⚡ Увеличивай майнинговую мощность\n' +
-        '🏆 Выполняй задания\n' +
-        '👥 Приглашай друзей\n\n' +
-        '⛏ Добывай будущие токены проекта прямо в Telegram.\n\n' +
-        '🔥 Начни строить свою майнинговую империю уже сейчас и получи шанс стать одним из первых обладателей <b>$MINE</b>!\n\n' +
-        '<b>Готов начать?</b>',
-      parse_mode: 'HTML',
-      reply_markup: Markup.inlineKeyboard([
-        [
-          Markup.button.webApp('⛏️ Начать', process.env.FRONTEND_URL),
-          Markup.button.url('📢 Официальный канал', process.env.CHANNEL)
-        ]
-      ])
-    }
-  );
+    // Используем replyWithPhoto и передаем caption (подпись к фото) с кнопками
+    await ctx.replyWithPhoto(
+        { url: 'https://raw.githubusercontent.com/Legenda3333/MiningEmpire-frontend/main/images/start_image.png' },
+        {
+            caption: 
+                '👋 <b>Добро пожаловать в MiningEmpire!</b> 🚀\n\n' +
+                '⚡ Увеличивай майнинговую мощность\n' +
+                '🏆 Выполняй задания\n' +
+                '👥 Приглашай друзей\n\n' +
+                '⛏ Добывай будущие токены проекта прямо в Telegram.\n\n' +
+                '🔥 Начни строить свою майнинговую империю уже сейчас и получи шанс стать одним из первых обладателей <b>$MINE</b>!\n\n' +
+                '<b>Готов начать?</b>',
+            parse_mode: 'HTML',
+            reply_markup: Markup.inlineKeyboard([
+                [Markup.button.webApp('⛏️ Начать', process.env.FRONTEND_URL)],
+                [Markup.button.url('📢 Официальный канал', process.env.CHANNEL)]
+            ])
+        }
+    );
 });
+
 
 // Обработка покупки
 bot.on('pre_checkout_query', async (ctx) => {
