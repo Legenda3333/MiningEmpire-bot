d2cf2aca9cdeb57cdd5e5a6e4dbf7615351dc18d
Артём <legenda333aa@gmail.com>
2026-03-09 20:24:15 +0300

    update

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const bot = new Telegraf(process.env.TOKEN);
 
 bot.start(async (ctx) => {
     await ctx.replyWithPhoto(
-        { source: 'start_image.png' },
+       { source: __dirname + '/start_image.png' },
         {
             caption: '👋 <b>Добро пожаловать в MiningEmpire!</b> 🚀\n\n' +
                      '⚡ Увеличивай майнинговую мощность\n' +
