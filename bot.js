c323908251c40d067670fc48dfc56cca5a54ede7
Артём <legenda333aa@gmail.com>
2026-03-09 21:22:35 +0300

    update

--- bot.js
+++ bot.js
@@ -7,24 +7,25 @@ const bot = new Telegraf(process.env.TOKEN);
 
 
 
-bot.start(async (ctx) => {
-    // Отправляем ссылку на фото, которое будет отображаться как preview
-    await ctx.replyWithHTML(
-        '<a href="https://raw.githubusercontent.com/Legenda3333/MiningEmpire-frontend/main/images/start_image.png">‌</a>' +
-        '👋 <b>Добро пожаловать в MiningEmpire!</b> 🚀\n\n' +
-        '⚡ Увеличивай майнинговую мощность\n' +
-        '🏆 Выполняй задания\n' +
-        '👥 Приглашай друзей\n\n' +
-        '⛏ Добывай будущие токены проекта прямо в Telegram.\n\n' +
-        '🔥 Начни строить свою майнинговую империю уже сейчас и получи шанс стать одним из первых обладателей <b>$MINE</b>!\n\n' +
-        '<b>Готов начать?</b>',
-        {
-            reply_markup: Markup.inlineKeyboard([
-                [Markup.button.webApp('⛏️ Начать', process.env.FRONTEND_URL)],
-                [Markup.button.url('📢 Официальный канал', process.env.CHANNEL)]
-            ])
-        }
-    );
+await ctx.telegram.sendMediaGroup(ctx.chat.id, [
+  {
+    type: 'photo',
+    media: 'https://raw.githubusercontent.com/Legenda3333/MiningEmpire-frontend/main/images/start_image.png',
+    caption:
+      '👋 <b>Добро пожаловать в MiningEmpire!</b> 🚀\n\n' +
+      '⚡ Увеличивай майнинговую мощность\n🏆 Выполняй задания\n👥 Приглашай друзей\n\n' +
+      '⛏ Добывай будущие токены проекта прямо в Telegram.\n\n' +
+      '🔥 Начни строить свою майнинговую империю уже сейчас и получи шанс стать одним из первых обладателей <b>$MINE</b>!\n\n' +
+      '<b>Готов начать?</b>',
+    parse_mode: 'HTML'
+  }
+], {
+  reply_markup: Markup.inlineKeyboard([
+    [
+      Markup.button.webApp('⛏️ Начать', process.env.FRONTEND_URL),
+      Markup.button.url('📢 Официальный канал', process.env.CHANNEL)
+    ]
+  ])
 });
 
 
